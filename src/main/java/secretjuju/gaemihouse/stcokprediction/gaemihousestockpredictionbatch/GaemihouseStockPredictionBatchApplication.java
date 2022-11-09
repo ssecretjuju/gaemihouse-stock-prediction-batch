4d53374c83e02016c6e4540e2832875be5f0a755
DonghwooCho <whehdgn1001@kookmin.ac.kr>
2022-11-09 14:45:00 +0900

    initial commit

---
+++ src/main/java/secretjuju/gaemihouse/stcokprediction/gaemihousestockpredictionbatch/GaemihouseStockPredictionBatchApplication.java
@@ -0,0 +1,13 @@
+package secretjuju.gaemihouse.stcokprediction.gaemihousestockpredictionbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaemihouseStockPredictionBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GaemihouseStockPredictionBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/secretjuju/gaemihouse/stcokprediction/gaemihousestockpredictionbatch/GaemihouseStockPredictionBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package secretjuju.gaemihouse.stcokprediction.gaemihousestockpredictionbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaemihouseStockPredictionBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
